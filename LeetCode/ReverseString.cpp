589486e89af9cedd8cf786d69b0f4b7d127c06ff
Dereck Tu <dereck.tu@hotmail.com>
2019-03-23 18:56:03 -0400

    Add leetcode solution to Reverse String

---
+++ LeetCode/ReverseString.cpp
@@ -0,0 +1,11 @@
+void helper(vector<char>& s,int i){
+    if(i<s.size()/2){
+        char temp = s[i];
+        s[i] = s[s.size()-i-1];
+        s[s.size()-i-1] = temp;
+        helper(s,i+1);
+    }
+}
+void reverseString(vector<char>& s) {
+    helper(s,0);
+}
