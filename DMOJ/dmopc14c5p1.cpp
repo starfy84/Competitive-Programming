446c4abf7004a32cd7ec0e62fa4caa4d6fdc770c
starfy84 <dereck.tu@hotmail.com>
2019-04-13 16:28:41 -0400

    Create dmopc14c5p1.cpp

---
+++ DMOJ/dmopc14c5p1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+    
+    int a, b;
+    cin>>a>>b;
+    
+    cout<<1.05*a*a*b;
+    return 0;
+}
